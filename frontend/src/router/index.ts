c496f6ea69034a6f9db466f008b5d4983feffc5f
Smaug6739 <veauville.raphael@gmail.com>
2022-08-26 09:50:18 +0200

    Reorganize routes

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -2,23 +2,7 @@ import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
 import publicRoutes from './public';
 import privateRoutes from './private';
 
-let routes: RouteRecordRaw[] = [
-  {
-    path: '/doc/:subject',
-    name: 'Home',
-    component: () => import('../views/Data.vue'),
-  },
-  {
-    path: '/doc/:subject/:category',
-    name: 'Home',
-    component: () => import('../views/Data.vue'),
-  },
-  {
-    path: '/doc/:subject/:category/:doc_name',
-    name: 'Home',
-    component: () => import('../views/Data.vue'),
-  },
-];
+let routes: RouteRecordRaw[] = [];
 
 routes = routes.concat(publicRoutes);
 routes = routes.concat(privateRoutes);
--- frontend/src/router/public.ts
+++ frontend/src/router/public.ts
@@ -2,6 +2,25 @@ import { type RouteRecordRaw } from 'vue-router';
 
 const routes: RouteRecordRaw[] = [
   {
+    path: '/doc/:subject',
+    name: 'Home documentation',
+    component: () => import('../views/Data.vue'),
+    children: [
+      {
+        path: ':category',
+        name: 'Category (main)',
+        component: () => import('../views/Data.vue'),
+        children: [
+          {
+            path: ':doc_name',
+            name: 'Subcategory',
+            component: () => import('../views/Data.vue'),
+          },
+        ],
+      },
+    ],
+  },
+  {
     path: '/login',
     name: 'Login',
     component: () => import('../views/admin/login.vue'),
