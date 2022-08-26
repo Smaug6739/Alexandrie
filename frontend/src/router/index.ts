6e2271d7b4c495ca52fa47d37c65f03bbc70adc8
Smaug6739 <veauville.raphael@gmail.com>
2022-08-26 13:04:46 +0200

    Authentification system

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -11,4 +11,35 @@ const router = createRouter({
   history: createWebHistory(),
   routes,
 });
+
+router.beforeEach((to, _, next) => {
+  function getCookie(cname: string) {
+    var name = cname + '=';
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+      var c: string = ca[i]!;
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return '';
+  }
+  const authenticated = getCookie('user_auth');
+  const isPublic = to.matched.some(record => record.meta.public);
+  if (!isPublic && !authenticated) {
+    return next({
+      path: '/login',
+      query: { redirect: to.fullPath },
+    });
+  }
+  if (authenticated && !isPublic) {
+    return next();
+  }
+  next();
+});
+
 export default router;
