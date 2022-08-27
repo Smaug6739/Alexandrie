5c0266d7e8fd7bcc0f356af4b989b746f07c1f4e
Smaug6739 <veauville.raphael@gmail.com>
2022-08-27 10:32:05 +0200

    File manager

--- frontend/src/views/admin/files/files.vue
+++ frontend/src/views/admin/files/files.vue
@@ -1,6 +1,17 @@
 <template>
 	<div>
-		<h1>Dashboard: Files manager</h1>
+		<form>
+			<fieldset>
+				<legend>Article</legend>
+				<label for="image">Image:</label>
+				<input type="file" placeholder="Image" name="image" id="file" />
+				<button type="button" class="btn btn-pink" @click="post">Post</button>
+				<p>Résultat: <code>{{ result }}</code> <button type="button" class="btn btn-pink " @click="copy">Copy
+						result</button>
+				</p>
+			</fieldset>
+		</form>
+
 	</div>
 
 </template>
@@ -8,5 +19,33 @@
 import { defineComponent } from 'vue';
 export default defineComponent({
 	name: 'admin-dashboard',
+	data() {
+		return {
+			result: 'Please upload file',
+		}
+	},
+	methods: {
+		async post() {
+			const body = new FormData();
+			const file = (document.getElementById("file") as HTMLInputElement).files![0]
+			if (!file) {
+				return;
+			}
+			body.append("file", file);
+
+			const responce = await fetch(`http://192.168.0.25:8082/api/v1/cdn/image`, {
+				method: "POST",
+				credentials: "include",
+				body: body,
+			});
+			const result = await responce.json();
+			if (result.status === "success") {
+				this.result = `http://192.168.0.25:8082/static${result.result}`;
+			}
+		},
+		copy() {
+			navigator.clipboard.writeText(this.result)
+		}
+	}
 });
 </script>
