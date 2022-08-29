ec53a64b5af02778bb9ad8bdfe6d7fa338482ed6
Veauville Raphael <veauville.raphael@gmail.com>
2022-08-29 15:56:20 +0200

    Fix improve size of large container

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -31,6 +31,8 @@ declare module '@vue/runtime-core' {
     $baseUrl: string;
   }
 }
+console.log(import.meta.env);
+
 app.config.globalProperties.$baseUrl = import.meta.env.VITE_BASE_API;
 
 app.mount('#app');
