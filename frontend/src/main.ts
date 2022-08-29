638c8cac9055a646402226eb7e6c8219d17213de
Veauville Raphael <veauville.raphael@gmail.com>
2022-08-29 11:28:48 +0200

    Deploy

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -25,13 +25,11 @@ app.component('Warning', Warning);
 app.use(router);
 app.use(store);
 
-console.log(import.meta.env);
-
 declare module '@vue/runtime-core' {
   interface ComponentCustomProperties {
     $baseUrl: string;
   }
 }
-app.config.globalProperties.$baseUrl = import.meta.env.BASE_API;
+app.config.globalProperties.$baseUrl = import.meta.env.VITE_BASE_API;
 
 app.mount('#app');
