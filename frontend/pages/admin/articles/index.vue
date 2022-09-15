9888a639384fbdc1c25ab1f7533032fb7765276b
Veauville Raphael <veauville.raphael@gmail.com>
2022-09-15 19:53:02 +0200

    Fixes

--- frontend/pages/admin/articles/index.vue
+++ frontend/pages/admin/articles/index.vue
@@ -5,90 +5,77 @@
 	</div>
 
 </template>
-<script lang="ts">
-import Datatable from '../../../components/common/DataTable.vue';
-import { useArticlesStore } from '../../../store';
-import { defineComponent } from 'vue';
-import { timestampToString } from '../../../helpers/date';
-import type { Article } from '../../../store';
-export default defineComponent({
-	name: 'admin-dashboard',
-	components: {
-		Datatable,
-	},
+<script lang="ts" setup>
+import Datatable from '@/components/common/DataTable.vue';
+import { useArticlesStore } from '@/store';
+import { timestampToString } from '@/helpers/date';
+import { storeToRefs } from 'pinia';
 
-	data() {
-		return {
-			articles: [] as Article[],
-			tableOptions: {
-				headers: [
-					{
-						title: 'Title',
-						key: 'title',
-						sortable: true,
-						textAlign: 'left',
-					},
-					{
-						title: 'Description (short)',
-						key: 'description',
-						sortable: true,
-						textAlign: 'left',
-					},
-					{
-						title: 'Created At',
-						key: 'created_at',
-						sortable: true,
-						textAlign: 'left',
-					},
-					{
-						title: 'Updated At',
-						key: 'updated_at',
-						sortable: true,
-						textAlign: 'left',
-					},
-					{
-						title: 'Actions',
-						key: 'actions',
-						sortable: false,
-						textAlign: 'left',
-					},
-				],
-				itemsPerPage: 10,
-			},
-		}
-	},
-	computed: {
-		displayArticles(): any {
-			return this.articles.map(article => {
-				return [
-					{
-						title: article.name,
-						action: 'text'
-					},
-					{
-						title: article.description,
-						action: 'text'
-					},
-					{
-						title: timestampToString(article.created_timestamp),
-						action: 'text'
-					},
-					{
-						title: timestampToString(article.updated_timestamp),
-						action: 'text'
-					},
-					{
-						title: '/admin/articles/edit-' + article.id,
-						action: 'link'
-					}
-				]
-			}).reverse();
+const articlesStore = useArticlesStore();
+const { getAll } = storeToRefs(articlesStore);
+
+const tableOptions = {
+	headers: [
+		{
+			title: 'Title',
+			key: 'title',
+			sortable: true,
+			textAlign: 'left',
 		},
-	},
-	async beforeMount() {
-		const store = useArticlesStore();
-		this.articles = store.getAll;
-	},
-});
+		{
+			title: 'Description (short)',
+			key: 'description',
+			sortable: true,
+			textAlign: 'left',
+		},
+		{
+			title: 'Created At',
+			key: 'created_at',
+			sortable: true,
+			textAlign: 'left',
+		},
+		{
+			title: 'Updated At',
+			key: 'updated_at',
+			sortable: true,
+			textAlign: 'left',
+		},
+		{
+			title: 'Actions',
+			key: 'actions',
+			sortable: false,
+			textAlign: 'left',
+		},
+	],
+	itemsPerPage: 10,
+}
+const displayArticles = computed(() => {
+	return getAll.value.map(article => {
+		return [
+			{
+				title: article.name,
+				action: 'text' as const,
+			},
+			{
+				title: article.description,
+				action: 'text' as const,
+			},
+			{
+				title: timestampToString(article.created_timestamp),
+				action: 'text' as const,
+			},
+			{
+				title: timestampToString(article.updated_timestamp),
+				action: 'text' as const,
+			},
+			{
+				title: '/admin/articles/edit-' + article.id,
+				action: 'link' as const,
+			}
+		]
+	}).reverse();
+})
+
+
 
 </script>
--- frontend/pages/admin/categories/index.vue
+++ frontend/pages/admin/categories/index.vue
@@ -4,73 +4,61 @@
 		<Datatable :rows="displayArticles" :headers="tableOptions.headers" />
 	</div>
 </template>
-<script lang="ts">
+<script lang="ts" setup>
 import Datatable from '../../../components/common/DataTable.vue';
 import { useCategoriesStore } from '../../../store';
-import { defineComponent } from 'vue';
-import type { Theme } from '../../../store';
-export default defineComponent({
-	name: 'admin-dashboard',
-	components: {
-		Datatable,
-	},
+import { computed } from 'vue';
 
-	data() {
-		return {
-			categories: [] as Theme[],
-			tableOptions: {
-				headers: [
-					{
-						title: 'Id',
-					},
-					{
-						title: 'Name',
-					},
-					{
-						title: 'Path',
-					},
-					{
-						title: 'Icon',
-					},
-					{
-						title: 'Actions',
-					},
-				],
-				itemsPerPage: 10,
-			},
-		}
-	},
-	computed: {
-		displayArticles(): any {
-			return this.categories.map(category => {
-				return [
-					{
-						title: category.id,
-						action: 'text'
-					},
-					{
-						title: category.name,
-						action: 'text'
-					},
-					{
-						title: `/${category.path}`,
-						action: 'text'
-					},
-					{
-						title: category.icon,
-						action: 'text'
-					},
-					{
-						title: '/admin/categories/edit-' + category.id,
-						action: 'link'
-					}
-				]
-			}).reverse();
+const { getAll: getCategories } = useCategoriesStore();
+
+const tableOptions = {
+	headers: [
+		{
+			title: 'Id',
 		},
-	},
-	async beforeMount() {
-		const store = useCategoriesStore();
-		this.categories = store.getAll;
-	},
+		{
+			title: 'Name',
+		},
+		{
+			title: 'Path',
+		},
+		{
+			title: 'Icon',
+		},
+		{
+			title: 'Actions',
+		},
+	],
+	itemsPerPage: 10,
+}
+
+const displayArticles = computed(() => {
+	return getCategories.map(category => {
+		return [
+			{
+				title: category.id,
+				action: 'text' as const,
+			},
+			{
+				title: category.name,
+				action: 'text' as const,
+			},
+			{
+				title: `/${category.path}`,
+				action: 'text' as const,
+			},
+			{
+				title: category.icon,
+				action: 'text' as const,
+			},
+			{
+				title: '/admin/categories/edit-' + category.id,
+				action: 'link' as const,
+			}
+		]
+	}).reverse();
 });
+
+
+
 </script>
