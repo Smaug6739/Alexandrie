2e1be3d3de87bf1c40fb06e165eca34301b5191c
Veauville Raphael <veauville.raphael@gmail.com>
2022-09-24 17:11:54 +0200

    New favicon

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,4 +1,5 @@
 export default defineNuxtConfig({
+  title: 'Scientia',
   vite: {
     css: {
       preprocessorOptions: {
@@ -13,5 +14,33 @@ export default defineNuxtConfig({
   modules: [
     // ...
     ['@pinia/nuxt'],
+  ],
+  meta: [
+    { name: 'robots', content: 'follow, index' },
+    { name: 'theme-color', content: '#00BFFF' },
+    {
+      name: 'description',
+      content: 'Site web qui regroupe mes notes/cours dans les domaines scientifiques.',
+    },
+
+    // Open Graph / Facebook
+    { property: 'og:type', content: 'website' },
+    { property: 'og:title', content: 'Scientia' },
+    {
+      property: 'og:description',
+      content: 'Site web qui regroupe mes notes/cours dans les domaines scientifiques.',
+    },
+    { property: 'og:url', content: `https://docs.smaug-6739.dev` },
+    { property: 'og:keywords', content: 'Cours, notes' },
+
+    // Twitter
+    { name: 'twitter:title', content: `Scientia` },
+    { name: 'twitter:creator', content: `@Smaug6739` },
+    { name: 'twitter:site', content: `@Smaug6739` },
+    { name: 'twitter:url', content: `https://docs.smaug-6739.dev` },
+    {
+      name: 'twitter:description',
+      content: `Site web qui regroupe mes notes/cours dans les domaines scientifiques.`,
+    },
   ],
 });
