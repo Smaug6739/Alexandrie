2d500fe09f7aec2524e489f5ce3b5904ab10230b
Veauville Raphael <veauville.raphael@gmail.com>
2022-09-25 09:54:39 +0200

    Fix SEO and config

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,5 +1,4 @@
 export default defineNuxtConfig({
-  title: 'Scientia',
   vite: {
     css: {
       preprocessorOptions: {
@@ -15,32 +14,42 @@ export default defineNuxtConfig({
     // ...
     ['@pinia/nuxt'],
   ],
-  meta: [
-    { name: 'robots', content: 'follow, index' },
-    { name: 'theme-color', content: '#00BFFF' },
-    {
-      name: 'description',
-      content: 'Site web qui regroupe mes notes/cours dans les domaines scientifiques.',
-    },
+  app: {
+    head: {
+      title: 'Scientia',
+      viewport: 'width=device-width, initial-scale=1',
+      charset: 'utf-8',
+      htmlAttrs: {
+        lang: 'en',
+      },
+      meta: [
+        { name: 'robots', content: 'follow, index' },
+        { name: 'theme-color', content: '#001e26' },
+        {
+          name: 'description',
+          content: 'Site web qui regroupe mes notes/cours dans les domaines scientifiques.',
+        },
 
-    // Open Graph / Facebook
-    { property: 'og:type', content: 'website' },
-    { property: 'og:title', content: 'Scientia' },
-    {
-      property: 'og:description',
-      content: 'Site web qui regroupe mes notes/cours dans les domaines scientifiques.',
-    },
-    { property: 'og:url', content: `https://docs.smaug-6739.dev` },
-    { property: 'og:keywords', content: 'Cours, notes' },
+        // Open Graph / Facebook
+        { property: 'og:type', content: 'website' },
+        { property: 'og:title', content: 'Scientia' },
+        {
+          property: 'og:description',
+          content: 'Site web qui regroupe mes notes/cours dans les domaines scientifiques.',
+        },
+        { property: 'og:url', content: `https://docs.smaug-6739.dev` },
+        { property: 'og:keywords', content: 'Cours, notes' },
 
-    // Twitter
-    { name: 'twitter:title', content: `Scientia` },
-    { name: 'twitter:creator', content: `@Smaug6739` },
-    { name: 'twitter:site', content: `@Smaug6739` },
-    { name: 'twitter:url', content: `https://docs.smaug-6739.dev` },
-    {
-      name: 'twitter:description',
-      content: `Site web qui regroupe mes notes/cours dans les domaines scientifiques.`,
+        // Twitter
+        { name: 'twitter:title', content: `Scientia` },
+        { name: 'twitter:creator', content: `@Smaug6739` },
+        { name: 'twitter:site', content: `@Smaug6739` },
+        { name: 'twitter:url', content: `https://docs.smaug-6739.dev` },
+        {
+          name: 'twitter:description',
+          content: `Site web qui regroupe mes notes/cours dans les domaines scientifiques.`,
+        },
+      ],
     },
-  ],
+  },
 });
