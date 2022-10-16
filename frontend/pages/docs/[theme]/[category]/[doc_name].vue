63341bc61467a1229883de4ee63eae545c25af32
Raphael <veauville.raphael@gmail.com>
2022-10-16 21:21:08 +0200

    Update

--- frontend/pages/docs/[theme]/[category]/[doc_name].vue
+++ frontend/pages/docs/[theme]/[category]/[doc_name].vue
@@ -1,41 +1,29 @@
 <template>
-	<div>
-		<main class="view view-medium">
-
-			<div v-if="article?.id">
-				<Transition name="fade" appear>
-					<div v-if="show">
-						<article v-html="article.content_html"></article>
-						<p class="sep">Dernière mise à jour le {{ timestampToString(article.updated_timestamp) }}</p>
-						<hr>
-						<div class="sep">
-							<span v-if="next" class="next">
-								<NuxtLink :to="`/docs/${next.main_category}/${next.sub_category}/${next.path}`">{{ next.name }}
-								</NuxtLink> →
-							</span>
-							<span v-if="previous" class="previous">
-								← <NuxtLink :to="`/docs/${previous.main_category}/${previous.sub_category}/${previous.path}`">
-									{{ previous.name }}
-								</NuxtLink>
-							</span>
-						</div>
-					</div>
-				</Transition>
-
+	<main class="view view-medium">
+		<Transition name="fade">
+			<div v-if="article?.id && show">
+				<article v-html="article.content_html"></article>
+				<p class="sep">Dernière mise à jour le {{ timestampToString(article.updated_timestamp) }}</p>
+				<hr>
+				<div class="sep">
+					<span v-if="next" class="next">
+						<NuxtLink :to="`/docs/${next.main_category}/${next.sub_category}/${next.path}`">{{ next.name }}
+						</NuxtLink> →
+					</span>
+					<span v-if="previous" class="previous">
+						← <NuxtLink :to="`/docs/${previous.main_category}/${previous.sub_category}/${previous.path}`">
+							{{ previous.name }}
+						</NuxtLink>
+					</span>
+				</div>
 			</div>
-
-			<div v-else>
-				<Loader msg="Loading" />
-			</div>
-
-		</main>
-	</div>
+		</Transition>
+	</main>
 </template>
 <script lang="ts" setup>
 import { useRoute } from "vue-router";
 import { timestampToString } from "@/helpers/date";
 import { useArticlesStore } from '@/store';
-import Loader from '@/components/Loader.vue';
 
 const route = useRoute();
 const articlesStore = useArticlesStore();
@@ -44,9 +32,7 @@ const show = ref(false);
 
 const article = computed(() => {
 	show.value = false;
-	setTimeout(() => {
-		show.value = true;
-	}, 200);
+	setTimeout(() => show.value = true, 100);
 	return articlesStore.getByPaths(route.params.doc_name as string, route.params.category as string, route.params.theme as string)
 });
 const next = computed(() => articlesStore.getNext(article.value));
