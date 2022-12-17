d30a14e6f21dc4a75b14be3500c6f1320b9a70b5
Veauville Raphael <veauville.raphael@gmail.com>
2022-12-17 09:27:35 +0100

    Update

--- backend/database/main_categories.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS `main_categories` (
-  `id` varchar(50) NOT NULL,
-  `name` varchar(100) DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  `order` int DEFAULT NULL,
-  `path` varchar(50) DEFAULT NULL,
-  `icon` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
