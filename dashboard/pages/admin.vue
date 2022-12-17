b394da2d20de61441216d3d30f49e500fa370b26
Veauville Raphael <veauville.raphael@gmail.com>
2022-12-17 15:25:00 +0100

    Fix dashboard

--- dashboard/pages/admin.vue
+++ dashboard/pages/admin.vue
@@ -3,18 +3,10 @@
 		<main class="view-large">
 			<admin-sidebar />
 			<h1>Dashboard: Admin</h1>
-			<p>
-
-			</p>
 			<NuxtPage />
 		</main>
 	</div>
 </template>
 <script lang="ts" setup>
 import adminSidebar from '@/components/layout/admin-sidebar/Sidebar.vue';
-import { useArticlesStore, useCategoriesStore } from '@/store';
-const store = useArticlesStore();
-const categoriesStore = useCategoriesStore();
-store.fetchArticles();
-categoriesStore.fetchCategories();
 </script>
