7db5c0a845ac5bfe8c4c73bd83a062b1c55c66a6
Veauville Raphael <veauville.raphael@gmail.com>
2022-12-19 20:23:14 +0100

    Fix

--- frontend/store/categories.store.ts
+++ frontend/store/categories.store.ts
@@ -36,7 +36,6 @@ export const useCategoriesStore = defineStore('categories', {
   },
   actions: {
     fetchCategories: async function () {
-      if (process.client) return; // only fetch on server side
       const { data, error } = await useLazyAsyncData<APIResult<Theme[]>>('categories', () => {
         return $fetch(`${baseUrl}/api/v1/categories`);
       });
