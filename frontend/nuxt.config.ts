820bb6c1fc91ad4893e386f04bf686b2eac0d8e1
Veauville Raphael <veauville.raphael@gmail.com>
2022-12-21 14:38:29 +0100

    Test

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -10,7 +10,7 @@ export default defineNuxtConfig({
   },
   routeRules: {
     // Enable Incremental Static Generation (ISG)
-    '/**': { swr: true }, //TODO: Not supported yet
+    '/**': { swr: 100 }, //TODO: Not supported yet
   },
   plugins: ['~/plugins/route', '~/plugins/pwa'],
   css: ['~/styles/main.scss'],
