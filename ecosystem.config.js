4ca35c5554641a1a4df2ea5dbd6c0d87876b0d8b
Raphael Veauville <veauville.raphael@gmail.com>
2024-03-12 23:18:41 +0100

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,9 +6,6 @@ module.exports = {
       script: 'node main.js',
       env: {
         NODE_ENV: 'production',
-        DATABASE_PASSWORD: process.env.DATABASE_PASSWORD,
-        DATABASE_USER: process.env.DATABASE_USERNAME,
-        JWT_SECRET: process.env.JWT_SECRET,
         PORT: 8201,
         DOMAIN_CLIENT: 'https://alexandrie-hub.fr',
         DOMAIN_DASHBOARD: 'https://dashboard.alexandrie-hub.fr',
