5fd7a6989e4cfd0a6615b71cb22f01e6b28d6d93
Veauville Raphael <veauville.raphael@gmail.com>
2024-03-31 21:38:31 +0200

    Fix deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: "alexandrie-backend",
       cwd: "./backend/dist",
-      script: "node main.js",
+      script: "main.js",
       interpreter: "~/.bun/bin/bun",
       env: {
         NODE_ENV: "production",
@@ -17,16 +17,17 @@ module.exports = {
       name: "alexandrie-frontend",
       cwd: "./frontend",
       interpreter: "~/.bun/bin/bun",
-      script: "PORT=8200 node .output/server/index.mjs",
+      script: ".output/server/index.mjs",
       env: {
         NODE_ENV: "production",
+        PORT: 8200,
       },
     },
     {
       name: "alexandrie-dashboard",
       cwd: "./dashboard",
       interpreter: "~/.bun/bin/bun",
-      script: "PORT=8202 node .output/server/index.mjs",
+      script: ".output/server/index.mjs",
       env: {
         NODE_ENV: "production",
       },
