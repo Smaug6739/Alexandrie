490d4463f76eb960a10ca4fb74004e93084a087b
Smaug6739 <veauville.raphael@gmail.com>
2024-07-09 14:59:51 +0200

    Fix auth log

--- frontend/middleware/authentification.global.ts
+++ frontend/middleware/authentification.global.ts
@@ -1,8 +1,6 @@
 export default defineNuxtRouteMiddleware((to, from) => {
   if (!import.meta.client) return;
   const user_auth = localStorage.getItem('isLoggedIn');
-  console.log(user_auth);
-
   if (to.fullPath.startsWith('/dashboard') && !user_auth) {
     return navigateTo({
       path: '/login',
