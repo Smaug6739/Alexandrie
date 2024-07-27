f87e33cf492fae916fcc12818f00a79353824966
Smaug6739 <veauville.raphael@gmail.com>
2024-07-27 19:56:13 +0200

    Fix backend dir

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'alexandrie-backend',
       cwd: './backend',
-      script: 'bun dist/main.js',
+      script: 'bun main.js',
       env: {
         NODE_ENV: 'production',
         PORT: 8201,
