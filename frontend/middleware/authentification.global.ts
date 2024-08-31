6c9a4f40abb30fa3ba8d3da79291b95ddf64fbf9
Smaug6739 <veauville.raphael@gmail.com>
2024-08-31 20:56:46 +0200

    Fix auth mid frontend (fix redirect loop)

--- frontend/middleware/authentification.global.ts
+++ frontend/middleware/authentification.global.ts
@@ -2,6 +2,13 @@ export default defineNuxtRouteMiddleware(async (to, _) => {
   if (!import.meta.client) return;
   const user_auth = localStorage.getItem('isLoggedIn');
 
+  if (to.fullPath.startsWith('/dashboard') && !user_auth) {
+    return navigateTo({
+      path: '/login',
+      query: { redirect: to.fullPath },
+    });
+  }
+
   if (to.fullPath.startsWith('/dashboard/admin')) {
     const user = await useUserStore().fetch();
     if (user && user.role === 2) return;
@@ -10,14 +17,7 @@ export default defineNuxtRouteMiddleware(async (to, _) => {
     });
   }
 
-  if (to.fullPath.startsWith('/dashboard') && !user_auth) {
-    return navigateTo({
-      path: '/login',
-      query: { redirect: to.fullPath },
-    });
-  }
-
-  if (to.fullPath.startsWith('/login') && user_auth) {
+  if (to.fullPath.startsWith('/login') || (to.fullPath.startsWith('/signup') && user_auth)) {
     return navigateTo({
       path: '/dashboard',
     });
