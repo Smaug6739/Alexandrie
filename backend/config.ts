9933ba9030b0497693b7269faa788cb546085af7
Raphael Veauville <veauville.raphael@gmail.com>
2024-12-23 09:03:19 +0100

    Update config.ts

--- backend/config.ts
+++ backend/config.ts
@@ -4,7 +4,7 @@ export const config = {
   max_file_size: 20 * 1024 * 1024, // 20MB
   upload_path: resolve(__dirname, '../uploads'),
   access_token_expiration: '1800s',
-  access_token_expiration_ms: 5 * 1000, // 30 minutes
+  access_token_expiration_ms: 1800 * 1000, // 30 minutes
   refresh_token_expiration: 7 * 24 * 60 * 60 * 1000,
   // in octets
   max_user_size: 1000 * 1024 * 1024, // 1GB
