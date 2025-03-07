d2a1f8ea3382d5d634e373751d18c5010f2e2c80
Smaug6739 <veauville.raphael@gmail.com>
2025-03-07 21:10:04 +0100

    Fix backend types

--- backend/config.ts
+++ backend/config.ts
@@ -1,9 +1,10 @@
 import { resolve } from 'path';
+import type ms from 'ms';
 
 export const config = {
   max_file_size: 20 * 1024 * 1024, // 20MB
   upload_path: resolve(__dirname, '../uploads'),
-  access_token_expiration: '1800s',
+  access_token_expiration: '1800s' as ms.StringValue,
   access_token_expiration_ms: 1800 * 1000, // 30 minutes
   refresh_token_expiration: 7 * 24 * 60 * 60 * 1000,
   // in octets
