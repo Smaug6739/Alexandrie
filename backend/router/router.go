6d3aea95cc309d68079addda6fa9ba3ae3d1ed1e
Smaug#6739 <veauville.raphael@gmail.com>
2025-03-21 20:25:45 +0100

    Init backend go

---
+++ backend/router/router.go
@@ -0,0 +1,19 @@
+package router
+
+import (
+	documents "Smaug6739/Alexandrie/router/routes"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Init() {
+	router := NewRouter()
+	router.Run("localhost:8080")
+}
+
+func NewRouter() *gin.Engine {
+	router := gin.New()
+	mainGroup := router.Group("/api")
+	documents.Routes(mainGroup)
+	return router
+}
