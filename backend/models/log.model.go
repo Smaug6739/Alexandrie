53f97647bd1526bd3d297d2130040886a83f663d
Smaug#6739 <veauville.raphael@gmail.com>
2025-04-07 18:33:04 +0200

    Fix ip addr no optional

--- backend/models/log.model.go
+++ backend/models/log.model.go
@@ -3,7 +3,7 @@ package models
 type Log struct {
 	Id        int64   `json:"id"`
 	UserId    int64   `json:"user_id"`
-	IpAddr    *string `json:"ip_address"`
+	IpAddr    string  `json:"ip_address"`
 	UserAgent *string `json:"user_agent"`
 	Location  string  `json:"location"`
 	Type      string  `json:"type"`
