028355538e754a383440ddb935085fa6b939c114
Smaug#6739 <veauville.raphael@gmail.com>
2025-04-08 09:36:26 +0200

    Document model

--- backend/models/document.model.go
+++ backend/models/document.model.go
@@ -2,15 +2,15 @@ package models
 
 type Document struct {
 	Id               int64   `json:"id" form:"id" binding:"omitempty"`
-	Name             string  `json:"name" `
-	Description      *string `json:"description"`
-	Tags             *string `json:"tags"`
-	Category         *int64  `json:"category"`
-	ParentId         *int64  `json:"parent_id"`
-	Accessibility    int     `json:"accessibility"`
-	ContentMarkdown  *string `json:"content_markdown"`
-	ContentHtml      *string `json:"content_html"`
-	AuthorId         int64   `json:"author_id"`
-	CreatedTimestamp int64   `json:"created_timestamp"`
-	UpdatedTimestamp int64   `json:"updated_timestamp"`
+	Name             string  `json:"name" form:"name" binding:"required,max=50"`
+	Description      *string `json:"description" form:"description" binding:"omitempty,max=250"`
+	Tags             *string `json:"tags" form:"tags" binding:"omitempty,max=250"`
+	Category         *int64  `json:"category" form:"category" binding:"omitempty"`
+	ParentId         *int64  `json:"parent_id" form:"parent_id" binding:"omitempty"`
+	Accessibility    int     `json:"accessibility" form:"accessibility" binding:"required"` // 0: Public; 1: Private; 2: Unlisted;
+	ContentMarkdown  *string `json:"content_markdown" form:"content_markdown"`
+	ContentHtml      *string `json:"content_html" form:"content_html"`
+	AuthorId         int64   `json:"author_id" form:"author_id" binding:"required"`
+	CreatedTimestamp int64   `json:"created_timestamp" form:"created_timestamp" binding:"omitempty"`
+	UpdatedTimestamp int64   `json:"updated_timestamp" form:"updated_timestamp" binding:"omitempty"`
 }
