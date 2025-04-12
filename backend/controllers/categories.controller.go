f70ba3bff68e89bf5de4e5b5a374892d24ae692e
Smaug#6739 <veauville.raphael@gmail.com>
2025-04-12 19:31:35 +0200

    Category controller

---
+++ backend/controllers/categories.controller.go
@@ -0,0 +1,39 @@
+package controllers
+
+import (
+	"alexandrie/app"
+	"alexandrie/utils"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type CategoriesController interface {
+	GetCategories(c *gin.Context) (int, any)
+}
+
+func NewCategoriesController(app *app.App) CategoriesController {
+	return &Controller{
+		app: app,
+	}
+}
+
+// Get documents
+// @Summary Get all documents
+// @Method GET
+// @Router /documents [get]
+// @Security Authenfification: Auth
+// @Success 200 {object} Success([]models.Document)
+// @Failure 400 {object} Error
+// @Failure 401 {object} Error
+func (ctr *Controller) GetCategories(c *gin.Context) (int, any) {
+	id, err := utils.SelfOrPermission(c, utils.ADMINISTRATOR, "userId")
+	if err != nil {
+		return http.StatusUnauthorized, err
+	}
+	documents, err := ctr.app.Services.Document.GetAllDocuments(id)
+	if err != nil {
+		return http.StatusInternalServerError, err
+	}
+	return http.StatusOK, documents
+}
