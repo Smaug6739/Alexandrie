26fa3c317c2a489041e31d5747d501549572addb
Smaug#6739 <veauville.raphael@gmail.com>
2025-04-16 08:45:18 +0200

    Add tests of permissions & user sustem

--- backend/tests/helpers.go
+++ backend/tests/helpers.go
@@ -12,8 +12,9 @@ import (
 const BaseURL = "http://localhost:8080/api"
 const ADMIN_USERNAME = "Smaug"
 const ADMIN_PASSWORD = "10082005"
-const USER_USERNAME = "Baltazar"
+const USER_USERNAME = "Balthazar"
 const USER_PASSWORD = "10082005"
+const USER_ID = "564704938298982405"
 
 type APIResponse struct {
 	Status     string `json:"status"`
@@ -26,7 +27,6 @@ type APIResponse struct {
 func DoPost(t *testing.T, client *http.Client, path string, body any) APIResponse {
 	var contentType string
 	var reader io.Reader
-
 	switch b := body.(type) {
 	case map[string]any:
 		jsonBody, err := json.Marshal(b)
@@ -75,6 +75,45 @@ func DoDelete(t *testing.T, client *http.Client, path string) APIResponse {
 	resp, err := client.Do(req)
 	if err != nil {
 		t.Fatalf("DELETE %s failed: %v", path, err)
+	}
+	val := ParseJSONResponse[APIResponse](t, resp)
+	val.StatusCode = resp.StatusCode
+	return val
+}
+
+func DoPatch(t *testing.T, client *http.Client, path string, body any) APIResponse {
+	var contentType string
+	var reader io.Reader
+
+	switch b := body.(type) {
+	case map[string]any:
+		jsonBody, err := json.Marshal(b)
+		if err != nil {
+			t.Fatalf("Erreur de marshal JSON: %v", err)
+		}
+		reader = bytes.NewReader(jsonBody)
+		if contentType == "" {
+			contentType = "application/json"
+		}
+	case string:
+		reader = strings.NewReader(b)
+		if contentType == "" {
+			contentType = "application/x-www-form-urlencoded"
+		}
+	case nil:
+		reader = nil
+	default:
+		t.Fatalf("Type de body non supporté: %T", body)
+	}
+	req, err := http.NewRequest(http.MethodPatch, BaseURL+path, reader)
+	if err != nil {
+		t.Fatalf("Error creating request: %v", err)
+	}
+	req.Header.Set("Content-Type", contentType)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		t.Fatalf("PATCH %s failed: %v", path, err)
 	}
 	val := ParseJSONResponse[APIResponse](t, resp)
 	val.StatusCode = resp.StatusCode
