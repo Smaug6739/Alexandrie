d97eba15295cefb2559337140c5bd4747f8bc85d
Smaug6739 <veauville.raphael@gmail.com>
2025-04-19 17:36:48 +0200

    Fix documents router

--- backend/router/routes/documents.go
+++ backend/router/routes/documents.go
@@ -15,7 +15,7 @@ func Documents(app *app.App, router *gin.RouterGroup) {
 
 	doc.GET("/:userId", middlewares.Auth(), utils.WP(docCtrl.GetDocuments))
 	doc.GET("/:userId/:id", middlewares.Auth(), utils.WP(docCtrl.GetDocument))
-	doc.POST("/", middlewares.Auth(), utils.WP(docCtrl.CreateDocument))
+	doc.POST("", middlewares.Auth(), utils.WP(docCtrl.CreateDocument))
 	doc.PUT("/:id", middlewares.Auth(), utils.WP(docCtrl.UpdateDocument))
 	doc.DELETE("/:id", middlewares.Auth(), utils.WP(docCtrl.DeleteDocument))
 }
