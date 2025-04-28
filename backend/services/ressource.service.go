761a366444c742f143c067da81b4ca77d7f99bca
Smaug6739 <veauville.raphael@gmail.com>
2025-04-28 21:48:21 +0200

    Set new ressources order

--- backend/services/ressource.service.go
+++ backend/services/ressource.service.go
@@ -21,7 +21,7 @@ func NewRessourceService(db *sql.DB) RessourceService {
 
 func (s *Service) GetAllUploadsByUserId(userId types.Snowflake) ([]*models.Ressource, error) {
 	var ressources = make([]*models.Ressource, 0)
-	rows, err := s.db.Query("SELECT * from ressources WHERE author_id = ?", userId)
+	rows, err := s.db.Query("SELECT * from ressources WHERE author_id = ? ORDER BY created_timestamp DESC", userId)
 	if err != nil {
 		return nil, err
 	}
