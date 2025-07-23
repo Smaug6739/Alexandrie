4c6e1191911bc3ad064c39338515f91eff919782
Smaug6739 <veauville.raphael@gmail.com>
2025-07-23 15:40:01 +0200

    Add link to github

--- frontend/app/pages/dashboard/settings/_views/about.vue
+++ frontend/app/pages/dashboard/settings/_views/about.vue
@@ -6,6 +6,12 @@
   <p>
     <strong>Client: {{ isPWA ? 'PWA' : 'Website' }}</strong>
   </p>
+  <p>
+    Alexandrie is a personal knowledge management tool designed to help you organize and retrieve your notes efficiently. <br />
+    It offers a range of features including document organization, tagging, and advanced search capabilities. <br />
+    If you have any questions, suggestions or if you found a bug, please feel free to reach out to us! You can create an issue on the
+    <NuxtLink to="https://github.com/Smaug6739/Alexandrie" target="_blank" style="color: var(--primary); font-weight: bold">GitHub repository</NuxtLink>.
+  </p>
 </template>
 
 <script setup lang="ts">
