06881ab1fc55e918b8ef9458b7abff1211572e9f
Smaug6739 <veauville.raphael@gmail.com>
2025-07-27 10:10:23 +0200

    Fix migration init

--- backend/migrations/000001_init.up.sql
+++ backend/migrations/000001_init.up.sql
@@ -90,6 +90,25 @@ CREATE TABLE IF NOT EXISTS `connections_logs` (
   CONSTRAINT `connections_logs_users_id_fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
 );
 
+
+CREATE TABLE IF NOT EXISTS `city_locations_fr` (
+  `geoname_id` int NOT NULL,
+  `locale_code` text,
+  `continent_code` text,
+  `continent_name` text,
+  `country_iso_code` text,
+  `country_name` text,
+  `subdivision_1_iso_code` text,
+  `subdivision_1_name` text,
+  `subdivision_2_iso_code` text,
+  `subdivision_2_name` text,
+  `city_name` text,
+  `metro_code` text,
+  `time_zone` text,
+  `is_in_european_union` text,
+  PRIMARY KEY (`geoname_id`)
+);
+
 CREATE TABLE IF NOT EXISTS `city_ipv4_complete` (
   `network_start_integer` bigint DEFAULT NULL,
   `network_last_integer` bigint DEFAULT NULL,
@@ -122,22 +141,4 @@ CREATE TABLE IF NOT EXISTS `city_ipv6_complete` (
   `is_anycast` text,
   KEY `City_IPv6_complete_City_Locations_fr_geoname_id_fk` (`geoname_id`),
   CONSTRAINT `City_IPv6_complete_City_Locations_fr_geoname_id_fk` FOREIGN KEY (`geoname_id`) REFERENCES `city_locations_fr` (`geoname_id`)
-);
-
-CREATE TABLE IF NOT EXISTS `city_locations_fr` (
-  `geoname_id` int NOT NULL,
-  `locale_code` text,
-  `continent_code` text,
-  `continent_name` text,
-  `country_iso_code` text,
-  `country_name` text,
-  `subdivision_1_iso_code` text,
-  `subdivision_1_name` text,
-  `subdivision_2_iso_code` text,
-  `subdivision_2_name` text,
-  `city_name` text,
-  `metro_code` text,
-  `time_zone` text,
-  `is_in_european_union` text,
-  PRIMARY KEY (`geoname_id`)
 );
