e6562f28752493078615c4e723e39827f09bf895
Smaug6739 <veauville.raphael@gmail.com>
2025-09-20 20:14:32 +0200

    Fix props order

--- frontend/app/pages/dashboard/docs/_components/DocumentCardHeaderActionRow.vue
+++ frontend/app/pages/dashboard/docs/_components/DocumentCardHeaderActionRow.vue
@@ -20,13 +20,13 @@
         />
       </svg>
     </NuxtLink>
-    <NuxtLink @click="openEditModal" v-if="nodeStore.hasPermissions(doc, 2)">
+    <NuxtLink v-if="nodeStore.hasPermissions(doc, 2)" @click="openEditModal">
       <Icon name="settings" :big="true" fill="var(--font-color)" />
     </NuxtLink>
-    <NuxtLink @click="openPermissionsModal" v-if="nodeStore.hasPermissions(doc, 0)">
+    <NuxtLink v-if="nodeStore.hasPermissions(doc, 0)" @click="openPermissionsModal">
       <Icon name="users" :big="true" fill="var(--font-color)" />
     </NuxtLink>
-    <NuxtLink @click="openDeleteModal" v-if="nodeStore.hasPermissions(doc, 3)">
+    <NuxtLink v-if="nodeStore.hasPermissions(doc, 3)" @click="openDeleteModal">
       <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
         <path
           d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"
