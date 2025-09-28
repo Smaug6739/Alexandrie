2d8524d0cbaa45d98d18f16bda633ada2e2f4d19
Smaug6739 <veauville.raphael@gmail.com>
2025-09-28 16:15:57 +0200

    Fix custom icons display in the sidebar

--- frontend/app/composables/useNodesTree.ts
+++ frontend/app/composables/useNodesTree.ts
@@ -4,8 +4,9 @@ let nodes: ComputedRef<Item<Node>[]> | Ref<Item<Node>[]> = ref([]);
 const structure = computed(() => new TreeStructure(nodes.value));
 
 const resolveIcon = (item: Node) => {
-  if (item.role === 1) return item.icon || 'workspace';
-  if (item.role === 2) return item.icon || (item.shared ? 'shared_folder' : 'folder');
+  if (item.icon) return item.icon;
+  if (item.role === 1) return 'workspace';
+  if (item.role === 2) return item.shared ? 'shared_folder' : 'folder';
   if (item.role === 3) return item.accessibility == 1 ? 'file' : item.accessibility == 2 ? 'draft' : 'shared_doc';
   if (item.role === 4) return item.metadata?.filetype == 'application/pdf' ? 'pdf' : 'image';
   return 'file';
