34b2e0283dee877e58327348501ef62b4f3ea2a4
Smaug6739 <veauville.raphael@gmail.com>
2025-10-27 16:08:38 +0100

    Fixes with docker: reduce size

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -1,8 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 echo "Building application with current environment variables..."
 echo "API: $VITE_BASE_API"
 echo "CDN: $VITE_BASE_CDN"
 echo "URL: $NUXT_PUBLIC_BASE_URL"
-bun run build
 echo "Starting Nitro server..."
-exec bun .output/server/index.mjs
+exec node .output/server/index.mjs
