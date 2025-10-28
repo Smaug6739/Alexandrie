fe042315ee747271876eef689ec7f3b402d7c00d
Smaug6739 <veauville.raphael@gmail.com>
2025-10-28 09:26:28 +0100

    Fix docker compose, deployment scripts and docker images

--- backend/main.go
+++ backend/main.go
@@ -3,12 +3,18 @@ package main
 import (
 	"alexandrie/server"
 	"fmt"
+	"os"
 )
 
 func main() {
 	server, application := server.SetupServer()
-	fmt.Println("Starting server on port:", application.Config.Port)
+	port := os.Getenv("BACKEND_PORT")
+	if port == "" {
+		fmt.Fprintln(os.Stderr, "Error: BACKEND_PORT environment variable not set")
+		os.Exit(1)
+	}
+	fmt.Println("Starting server on port:", port)
 	defer application.DB.Close()
 
-	server.Run(":" + fmt.Sprintf("%d", application.Config.Port))
+	server.Run(":" + fmt.Sprintf("%s", port))
 }
--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "Building application with current environment variables..."
-echo "API: $VITE_BASE_API"
-echo "CDN: $VITE_BASE_CDN"
+echo "API: $NUXT_PUBLIC_BASE_API"
+echo "CDN: $NUXT_PUBLIC_BASE_CDN"
 echo "URL: $NUXT_PUBLIC_BASE_URL"
 echo "Starting Nitro server..."
 exec node .output/server/index.mjs
