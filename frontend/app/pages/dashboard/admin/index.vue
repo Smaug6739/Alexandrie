c7839aff47a2d659c61b1d3c78c57e569c6b9a43
Smaug6739 <veauville.raphael@gmail.com>
2025-10-28 18:20:46 +0100

    Add description to the admin dashboard

--- frontend/app/pages/dashboard/admin/index.vue
+++ frontend/app/pages/dashboard/admin/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="card-component">
     <h1>Admin dashboard</h1>
+    <p>From this dashboard, you can manage users and site settings.</p>
     <AppButton type="primary" @click="useRouter().push('/dashboard/admin/users')">Manage users</AppButton>
   </div>
 </template>
