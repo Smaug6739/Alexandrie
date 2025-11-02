50e26a7e8db1d33eb0a9155e8ff794bb4eaa821f
Smaug6739 <veauville.raphael@gmail.com>
2025-11-02 20:12:21 +0100

    Fix user logout when the access token is invalid

--- backend/router/routes/auth.go
+++ backend/router/routes/auth.go
@@ -17,6 +17,6 @@ func Auth(app *app.App, router *gin.RouterGroup) {
 	auth.POST("/refresh", utils.WP(authCtrl.RefreshSession))
 	auth.POST("/request-reset", utils.WP(authCtrl.RequestResetPassword))
 	auth.POST("/reset-password", utils.WP(authCtrl.ResetPassword))
-	auth.POST("/logout", middlewares.Auth(), utils.WP(authCtrl.Logout))
+	auth.POST("/logout", utils.WP(authCtrl.Logout))
 	auth.POST("/logout/all", middlewares.Auth(), utils.WP(authCtrl.LogoutAllDevices))
 }
