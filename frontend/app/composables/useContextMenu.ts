262fc5cb14f9631cc32dc4c217d2da2391eeaf0e
Smaug6739 <veauville.raphael@gmail.com>
2025-11-06 14:58:40 +0100

    Remove old french comments

--- frontend/app/composables/useContextMenu.ts
+++ frontend/app/composables/useContextMenu.ts
@@ -15,15 +15,9 @@ const menu = ref<ContextMenu | null>(null);
 
 export function useContextMenu() {
   function open(component: object, event: MouseEvent, options: ContextMenuOptions = {}) {
-    console.log('Opening context menu', component, event, options);
-    // Fermer d’abord si un autre est déjà ouvert
     close();
-
-    // Calcul position (éviter de dépasser l’écran)
     const { clientX: x, clientY: y } = event;
     menu.value = { component, x, y, options };
-
-    // Empêche le clic d’ouvrir un menu par défaut
     event.preventDefault();
   }
 
