fbb0c0cca54107c26e15794f4592ab45699922b9
Smaug6739 <veauville.raphael@gmail.com>
2025-11-19 18:30:34 +0100

    Fix icon size on android

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -72,32 +72,38 @@ export default defineNuxtConfig({
       theme_color: '#3956e7',
       icons: [
         {
-          src: '/icons/maskable_icon_x48.png',
-          sizes: '48x48',
-          type: 'image/png',
-          purpose: 'maskable',
-        },
-        {
-          src: '/icons/maskable_icon_x72.png',
-          sizes: '72x72',
-          type: 'image/png',
-          purpose: 'maskable',
-        },
-        {
-          src: '/icons/maskable_icon_x96.png',
-          sizes: '96x96',
-          type: 'image/png',
-          purpose: 'maskable',
-        },
-        {
-          src: '/icons/maskable_icon_x144.png',
+          src: '/icons/icon-m-144.png',
           sizes: '144x144',
           type: 'image/png',
           purpose: 'maskable',
         },
         {
-          src: '/android-chrome-192x192.png',
+          src: '/icons/icon-m-192.png',
           sizes: '192x192',
+          type: 'image/png',
+          purpose: 'maskable',
+        },
+        {
+          src: '/icons/icon-m-512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'maskable',
+        },
+        {
+          src: '/icons/icon-144.png',
+          sizes: '144x144',
+          type: 'image/png',
+          purpose: 'any',
+        },
+        {
+          src: '/icons/icon-192.png',
+          sizes: '192x192',
+          type: 'image/png',
+          purpose: 'any',
+        },
+        {
+          src: '/icons/icon-512.png',
+          sizes: '512x512',
           type: 'image/png',
           purpose: 'any',
         },
