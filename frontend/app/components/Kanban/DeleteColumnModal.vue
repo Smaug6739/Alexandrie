ec5510d901fe7c199c0d4e72d9b0bd0c7edd61b7
Smaug6739 <veauville.raphael@gmail.com>
2025-12-14 21:09:05 +0100

    Start new Kanban view system

---
+++ frontend/app/components/Kanban/DeleteColumnModal.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="modal">
+    <h3>Delete column</h3>
+    <p>Are you sure you want to delete the column "{{ columnTitle }}" ?</p>
+    <p v-if="cardCount > 0" class="warn">
+      This column contains {{ cardCount }} document{{ cardCount > 1 ? 's' : '' }}. They will be moved to the first column.
+    </p>
+    <p v-else class="info">This column is empty.</p>
+    <div class="footer">
+      <AppButton type="secondary" @click="emit('close')">Cancel</AppButton>
+      <AppButton type="danger" @click="confirmDelete">Delete</AppButton>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  columnTitle: string;
+  cardCount: number;
+  onConfirm: () => void;
+}>();
+
+const emit = defineEmits<{
+  close: [];
+}>();
+
+const confirmDelete = () => {
+  props.onConfirm();
+  emit('close');
+};
+</script>
+
+<style scoped lang="scss">
+.warn {
+  color: var(--red-dark);
+  opacity: 0.8;
+}
+
+.info {
+  color: var(--font-color-light);
+  font-style: italic;
+}
+</style>
