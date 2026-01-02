0c072958ba7d84ceffea395f1b9cb39ec3340325
Smaug6739 <veauville.raphael@gmail.com>
2026-01-02 18:15:06 +0100

    Adjust default notification

--- frontend/app/components/Notification.vue
+++ frontend/app/components/Notification.vue
@@ -17,8 +17,8 @@
         </svg>
         <svg v-else viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
           <circle cx="12" cy="12" r="10" />
-          <line x1="12" y1="16" x2="12" y2="12" />
-          <line x1="12" y1="8" x2="12.01" y2="8" />
+          <line x1="12" y1="16" x2="12" y2="11" />
+          <circle cx="12" cy="8" r="0.2" />
         </svg>
       </div>
       <div class="content">
