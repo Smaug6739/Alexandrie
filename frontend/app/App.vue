1a1e7c5a7167c5e66dd4d2bc57cce20e897b22d0
Smaug6739 <veauville.raphael@gmail.com>
2026-01-28 17:35:04 +0100

    fix app margins & size

--- frontend/app/App.vue
+++ frontend/app/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="app">
     <ModalManager />
     <ContextMenuManager />
     <NuxtPage />
@@ -25,3 +25,10 @@ watch(
   { immediate: true },
 );
 </script>
+
+<style lang="scss">
+.app {
+  height: 100vh;
+  overflow-y: auto;
+}
+</style>
