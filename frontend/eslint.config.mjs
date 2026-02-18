750b020d58bd7b34a45b76e9d142db9308b32323
Smaug6739 <veauville.raphael@gmail.com>
2026-02-18 18:34:28 +0100

    chore: add eslint perfectionist to sort object by keys

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,18 +1,72 @@
+import prettier from 'eslint-config-prettier';
+import perfectionist from 'eslint-plugin-perfectionist';
+
 // @ts-check
 import withNuxt, { defineFlatConfigs } from './.nuxt/eslint.config.mjs';
-import prettier from 'eslint-config-prettier';
 
 export default withNuxt(
   defineFlatConfigs(
     {
       ignores: ['app/styles/katex/**'],
     },
+
     {
+      plugins: {
+        perfectionist,
+      },
+
       rules: {
+        'no-console': ['warn', { allow: ['warn', 'error'] }],
+        'no-duplicate-imports': 'error',
+        'no-var': 'error',
+        'perfectionist/sort-imports': [
+          'error',
+          {
+            newlinesBetween: 1,
+            order: 'asc',
+            type: 'alphabetical',
+          },
+        ],
+
+        'perfectionist/sort-interfaces': [
+          'error',
+          {
+            order: 'asc',
+            type: 'alphabetical',
+          },
+        ],
+
+        'perfectionist/sort-objects': [
+          'error',
+          {
+            ignoreCase: false,
+            order: 'asc',
+            type: 'alphabetical',
+          },
+        ],
+
+        'perfectionist/sort-union-types': [
+          'error',
+          {
+            order: 'asc',
+            type: 'alphabetical',
+          },
+        ],
+
+        'prefer-const': 'error',
+
+        'vue/block-lang': [
+          'error',
+          {
+            script: { lang: 'ts' },
+          },
+        ],
+        'vue/component-name-in-template-casing': ['error', 'PascalCase'],
         'vue/multi-word-component-names': 'off',
         'vue/no-unused-properties': 'error',
       },
     },
+
     prettier,
   ),
 );
