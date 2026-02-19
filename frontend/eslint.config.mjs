cc6510ecdb35e4795d7b85987aa387ec904fd6f3
Smaug6739 <veauville.raphael@gmail.com>
2026-02-19 15:39:07 +0100

    chore: simplify eslint config to keep code clean

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,69 +1,31 @@
 import prettier from 'eslint-config-prettier';
-import perfectionist from 'eslint-plugin-perfectionist';
 
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs';
 
 export default withNuxt(
-    prettier,
-    {
-      ignores: ['app/styles/vendors/katex/**'],
+  prettier,
+  {
+    ignores: ['app/styles/vendors/katex/**'],
+  },
+
+  {
+    plugins: {},
+
+    rules: {
+      // 'no-console': ['warn', { allow: ['warn', 'error'] }],
+      'no-duplicate-imports': 'error',
+      'no-var': 'error',
+      'prefer-const': 'error',
+      'vue/block-lang': [
+        'error',
+        {
+          script: { lang: 'ts' },
+        },
+      ],
+      'vue/component-name-in-template-casing': ['error', 'PascalCase'],
+      'vue/multi-word-component-names': 'off',
+      'vue/no-unused-properties': 'error',
     },
-
-    {
-      plugins: {
-        perfectionist,
-      },
-
-      rules: {
-        'no-console': ['warn', { allow: ['warn', 'error'] }],
-        'no-duplicate-imports': 'error',
-        'no-var': 'error',
-        'perfectionist/sort-imports': [
-          'error',
-          {
-            newlinesBetween: 1,
-            order: 'asc',
-            type: 'alphabetical',
-          },
-        ],
-
-        'perfectionist/sort-interfaces': [
-          'error',
-          {
-            order: 'asc',
-            type: 'alphabetical',
-          },
-        ],
-
-        /*'perfectionist/sort-objects': [
-          'error',
-          {
-            ignoreCase: false,
-            order: 'asc',
-            type: 'alphabetical',
-          },
-        ],*/
-
-        'perfectionist/sort-union-types': [
-          'error',
-          {
-            order: 'asc',
-            type: 'alphabetical',
-          },
-        ],
-
-        'prefer-const': 'error',
-
-        'vue/block-lang': [
-          'error',
-          {
-            script: { lang: 'ts' },
-          },
-        ],
-        'vue/component-name-in-template-casing': ['error', 'PascalCase'],
-        'vue/multi-word-component-names': 'off',
-        'vue/no-unused-properties': 'error',
-      },
-    },
+  },
 );
