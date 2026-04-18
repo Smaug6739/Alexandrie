0cef8ec9b733060d86e77630aca0e0add66dead9
Smaug6739 <veauville.raphael@gmail.com>
2026-04-18 09:28:06 +0200

    fix: increase maxEntries for cache expiration plugin from 100 to 500

--- frontend/app/sw.ts
+++ frontend/app/sw.ts
@@ -27,7 +27,7 @@ registerRoute(
           statuses: [0, 200],
         }),
         new ExpirationPlugin({
-          maxEntries: 100,
+          maxEntries: 500,
           maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
         }),
       ],
