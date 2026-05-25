6628743a3c4431269c28571c3713b8cb06c16439
Nyveil <github@nyveil.eu>
2026-05-25 13:13:47 +0200

    Add German localization for CDN file management

---
+++ frontend/i18n/locales/de/cdn.ts
@@ -0,0 +1,51 @@
+export default {
+  actions: {
+    download: 'Datei herunterladen',
+    upload: {
+      idle: 'Hochladen',
+      multiple: '{n} Datei hochladen | {n} Dateien hochladen',
+      progress: 'Lade {n} / {total} Dateien hoch',
+    },
+  },
+
+  appdrop: {
+    addMore: 'Mehr Dateien hinzufügen',
+    link: 'hier klicken',
+    max: 'Maximal {n} Dateien',
+    prompt: 'Ziehe deine Datei hierher oder klicke auf {link}, um sie auszuwählen',
+    promptPlural: 'Ziehe deine Dateien hierher oder klicke auf {link}, um sie auszuwählen',
+    removeFile: 'Datei entfernen',
+  },
+
+  edit: {
+    description: 'Verwalte Ressourcen und Dateien auf dem Server. Du kannst Metadaten bearbeiten und Dateien löschen.',
+    title: 'Datei aktualisieren',
+  },
+
+  labels: {
+    originalPath: 'Ursprünglicher Pfad',
+    path: 'Pfad',
+  },
+
+  meta: {
+    shortTitle: 'CDN',
+    title: 'Dateimanager',
+  },
+
+  notifications: {
+    error: 'Das Hochladen von {file} ist fehlgeschlagen: {error}',
+    successMsg: '{n} Datei erfolgreich hochgeladen | {n} Datein erfolgreich hochgeladen',
+    successTitle: 'Hochladen abgeschlossen',
+  },
+
+  page: {
+    empty: 'Es wurden noch keine Dateien hochgeladen.',
+  },
+
+	preview: {
+    title: 'Vorschau',
+    unavailable: 'Für diesen Dateityp ist keine Vorschau verfügbar',
+  },
+
+  storageUsed: 'Belegter Speicherplatz',
+};
