0a6efe997cfeb107437936db4b8c213a997b5e24
Nyveil <github@nyveil.eu>
2026-05-26 23:08:23 +0200

    Add German translations for account deletion messages

---
+++ frontend/i18n/locales/de/user.ts
@@ -0,0 +1,7 @@
+export default {
+  accontDeletion: {
+    description: 'Du bist dabei, dein Konto zu löschen.',
+    title: 'Konto löschen',
+    waitModal: 'Bitte warte {seconds} Sekunde(n), bevor du dein Konto löschen kannst.',
+  },
+};
