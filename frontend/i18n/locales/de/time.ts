a37e528a458f74f9f2a58e2665de1d294e2aec83
Nyveil <github@nyveil.eu>
2026-05-26 23:07:01 +0200

    Add German time localization strings

---
+++ frontend/i18n/locales/de/time.ts
@@ -0,0 +1,9 @@
+export default {
+  daysAgo: 'Vor {count} Tag(e)',
+  hoursAgo: 'Vor {count} Stunde(n)',
+  justNow: 'Gerade eben',
+  minutesAgo: 'Vor {count} Minute(n)',
+  today: 'Heute',
+  weeksAgo: 'Vor {count} Woche',
+  yesterday: 'Gestern',
+};
