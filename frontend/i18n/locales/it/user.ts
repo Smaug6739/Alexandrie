65c459ab7d7835262ca0985e6d4e4fe7cf6856c1
albanobattistella <34811668+albanobattistella@users.noreply.github.com>
2026-07-11 19:14:51 +0200

    Add Italian translations for account deletion

---
+++ frontend/i18n/locales/it/user.ts
@@ -0,0 +1,7 @@
+export default {
+  accontDeletion: {
+    description: 'Stai per eliminare il tuo account.',
+    title: 'Elimina account',
+    waitModal: 'Attendi {seconds} secondi prima di poter eliminare il tuo account.',
+  },
+};
